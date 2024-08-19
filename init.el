af140e6088673c6f04cff0d0ccadf5b718b8cbdb
Ádran Farias Carnavale <adran.carnavale@gmail.com>
2024-08-18 23:11:06 -0300

    makes config more straightforward

--- init.el
+++ init.el
@@ -1,23 +1,29 @@
-;;; init.el --- Init -*- no-byte-compile: t; lexical-binding: t; -*-
+(setq read-process-output-max (* 4 1024 1024))  ; 4mb
+(setq gc-cons-threshold most-positive-fixnum)
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (setq gc-cons-threshold (* 100 1024 1024))))
 
-;; Author: James Cherti
-;; URL: https://github.com/jamescherti/minimal-emacs.d
-;; Package-Requires: ((emacs "29.1"))
-;; Keywords: maint
-;; Version: 1.0.2
-;; SPDX-License-Identifier: GPL-3.0-or-later
+(setq custom-file (locate-user-emacs-file "custom.el"))
+(load custom-file 'noerror 'nomessage)
 
-;;; Commentary:
-;; This is the main initialization file for Emacs. It configures package
-;; archives, ensures essential packages like `use-package` are installed, and
-;; sets up further package management and customization settings.
+(setq use-file-dialog nil)
+(setq use-dialog-box nil)
 
-;;; Code:
+(setq package-enable-at-startup t)
+(setq package-quickstart nil)
+(setq use-package-always-ensure t)
 
-;;; Load pre-init.el
-(minimal-emacs-load-user-init "pre-init.el")
+;; Emacs comes with several built-in packages, such as Org-mode, that are
+;; essential for many users. However, these built-in packages are often not the
+;; latest versions available. Ensure that your built-in packages are always up
+;; to date with:
+(setq package-install-upgrade-built-in t)
 
-;;; package.el
+(if (boundp 'use-short-answers)
+    (setq use-short-answers t)
+  (advice-add #'yes-or-no-p :override #'y-or-n-p))
+(defalias #'view-hello-file #'ignore)  ; Never show the hello file
 
 (require 'package)
 
@@ -53,287 +59,151 @@
 (eval-when-compile
   (require 'use-package))
 
-;;; Minibuffer
-;; Allow nested minibuffers
-(setq enable-recursive-minibuffers t)
-
-;; Keep the cursor out of the read-only portions of the.minibuffer
-(setq minibuffer-prompt-properties
-      '(read-only t intangible t cursor-intangible t face
-                  minibuffer-prompt))
-(add-hook 'minibuffer-setup-hook #'cursor-intangible-mode)
-
-;;; Misc
-
-;; switch-to-buffer runs pop-to-buffer-same-window instead
-(setq switch-to-buffer-obey-display-actions t)
-
-(setq show-paren-delay 0.1
-      show-paren-highlight-openparen t
-      show-paren-when-point-inside-paren t
-      show-paren-when-point-in-periphery t)
-
-(setq whitespace-line-column nil)  ; whitespace-mode
-
-;; I reduced the default value of 9 to simplify the font-lock keyword,
-;; aiming to improve performance. This package helps differentiate
-;; nested delimiter pairs, particularly in languages with heavy use of
-;; parentheses.
-(setq rainbow-delimiters-max-face-count 5)
-
-;; Can be activated with `display-line-numbers-mode'
-(setq-default display-line-numbers-width 3)
-(setq-default display-line-numbers-widen t)
-
-(setq comint-prompt-read-only t)
-(setq comint-buffer-maximum-size 2048)
-
-(setq compilation-always-kill t
-      compilation-ask-about-save nil
-      compilation-scroll-output 'first-error)
-
-(setq truncate-string-ellipsis "…")
-
-;; Configure Emacs to ask for confirmation before exiting
-(setq confirm-kill-emacs 'y-or-n-p)
-
-;;; Files
-
-;; Disable the warning "X and Y are the same file". Ignoring this warning is
-;; acceptable since it will redirect you to the existing buffer regardless.
-(setq find-file-suppress-same-file-warnings t)
-
-;; Resolve symlinks when opening files, so that any operations are conducted
-;; from the file's true directory (like `find-file').
-(setq find-file-visit-truename t
-      vc-follow-symlinks t)
-
-;; Skip confirmation prompts when creating a new file or buffer
-(setq confirm-nonexistent-file-or-buffer nil)
-
-(setq uniquify-buffer-name-style 'forward)
-
 (setq mouse-yank-at-point t)
 
-;; Prefer vertical splits over horizontal ones
-;; (setq split-width-threshold 170
-;;       split-height-threshold nil)
-(setq split-width-threshold 80
-      split-height-threshold 40)
-
-;; The native border "uses" a pixel of the fringe on the rightmost
-;; splits, whereas `window-divider` does not.
-(setq window-divider-default-bottom-width 1
-      window-divider-default-places t
-      window-divider-default-right-width 1)
-
-(add-hook 'after-init-hook #'window-divider-mode)
-
-;;; Backup files
-
-;; Avoid generating backups or lockfiles to prevent creating world-readable
-;; copies of files.
-(setq create-lockfiles nil)
-(setq make-backup-files nil)
-
-(setq backup-directory-alist
-      `(("." . ,(expand-file-name "backup" user-emacs-directory))))
-(setq tramp-backup-directory-alist backup-directory-alist)
-(setq backup-by-copying-when-linked t)
-(setq backup-by-copying t)  ; Backup by copying rather renaming
-(setq delete-old-versions t)  ; Delete excess backup versions silently
-(setq version-control t)  ; Use version numbers for backup files
-(setq kept-new-versions 5)
-(setq kept-old-versions 5)
-(setq vc-make-backup-files nil)  ; Do not backup version controlled files
-
-;;; Auto save
-;; Enable auto-save to safeguard against crashes or data loss. The
-;; `recover-file' or `recover-session' functions can be used to restore
-;; auto-saved data.
-(setq auto-save-default t)
-
-;; Do not auto-disable auto-save after deleting large chunks of
-;; text. The purpose of auto-save is to provide a failsafe, and
-;; disabling it contradicts this objective.
-(setq auto-save-include-big-deletions t)
-
-(setq auto-save-list-file-prefix
-      (expand-file-name "autosave/" user-emacs-directory))
-(setq tramp-auto-save-directory
-      (expand-file-name "tramp-autosave/" user-emacs-directory))
-
-;; Auto save options
-(setq kill-buffer-delete-auto-save-files t)
-
-;;; Auto revert
-;; Auto-revert in Emacs is a feature that automatically updates the
-;; contents of a buffer to reflect changes made to the underlying file
-;; on disk.
-(setq revert-without-query (list ".")  ; Do not prompt
-      auto-revert-stop-on-user-input nil
-      auto-revert-verbose t)
-
-;; Revert other buffers (e.g, Dired)
-(setq global-auto-revert-non-file-buffers t)
-
-;;; recentf
-;; `recentf' is an Emacs package that maintains a list of recently
-;; accessed files, making it easier to reopen files you have worked on
-;; recently.
-(setq recentf-max-saved-items 300) ; default is 20
-(setq recentf-auto-cleanup 'mode)
-
-;;; saveplace
-;; `save-place-mode` enables Emacs to remember the last location within a file
-;; upon reopening. This feature is particularly beneficial for resuming work at
-;; the precise point where you previously left off.
-(setq save-place-file (expand-file-name "saveplace" user-emacs-directory))
-(setq save-place-limit 600)
-
-;;; savehist
-;; `savehist` is an Emacs feature that preserves the minibuffer history between
-;; sessions. It saves the history of inputs in the minibuffer, such as commands,
-;; search strings, and other prompts, to a file. This allows users to retain
-;; their minibuffer history across Emacs restarts.
-(setq history-length 300)
-(setq savehist-save-minibuffer-history t)  ;; Default
-
-;;; Frames and windows
-
-;; Resizing the Emacs frame can be costly when changing the font. Disable this
-;; to improve startup times with fonts larger than the system default.
-(setq frame-resize-pixelwise t)
-
-;; However, do not resize windows pixelwise, as this can cause crashes in some
-;; cases when resizing too many windows at once or rapidly.
-(setq window-resize-pixelwise nil)
-
-(setq resize-mini-windows 'grow-only)
-
-;;; Smooth scrolling
-;; Enables faster scrolling through unfontified regions. This may result in
-;; brief periods of inaccurate syntax highlighting immediately after scrolling,
-;; which should quickly self-correct.
-(setq fast-but-imprecise-scrolling t)
-
-(setq hscroll-margin 2
-      hscroll-step 1
-      ;; Emacs spends excessive time recentering the screen when the cursor
-      ;; moves more than N lines past the window edges (where N is the value of
-      ;; `scroll-conservatively`). This can be particularly slow in larger files
-      ;; during extensive scrolling. If `scroll-conservatively` is set above
-      ;; 100, the window is never automatically recentered. The default value of
-      ;; 0 triggers recentering too aggressively. Setting it to 10 reduces
-      ;; excessive recentering and only recenters the window when scrolling
-      ;; significantly off-screen.
-      scroll-conservatively 10
-      scroll-margin 0
-      scroll-preserve-screen-position t
-      ;; Reduce cursor lag by preventing automatic adjustments to
-      ;; `window-vscroll' for unusually long lines. Setting
-      ;; `auto-window-vscroll' it to nil also resolves the issue of random
-      ;; half-screen jumps during scrolling.
-      auto-window-vscroll nil
-      ;; Mouse
-      mouse-wheel-scroll-amount '(1 ((shift) . hscroll))
-      mouse-wheel-scroll-amount-horizontal 1)
-
-;;; Cursor
-;; The blinking cursor is distracting and interferes with cursor settings in
-;; some minor modes that try to change it buffer-locally (e.g., Treemacs).
-;; Additionally, it can cause freezing, especially on macOS, for users with
-;; customized and colored cursors.
-(blink-cursor-mode -1)
-
-;; Don't blink the paren matching the one at point, it's too distracting.
-(setq blink-matching-paren nil)
-
-;; Don't stretch the cursor to fit wide characters, it is disorienting,
-;; especially for tabs.
-(setq x-stretch-cursor nil)
-
-;;; Annoyances
-
-;; No beeping or blinking
 (setq visible-bell nil)
 (setq ring-bell-function #'ignore)
 
-;; This controls how long Emacs will blink to show the deleted pairs with
-;; `delete-pair'. A longer delay can be annoying as it causes a noticeable pause
-;; after each deletion, disrupting the flow of editing.
-(setq delete-pair-blink-delay 0.1)
-
-;;; Indent and formatting
-(setq-default left-fringe-width  8)
-(setq-default right-fringe-width 8)
-
-;; Do not show an arrow at the top/bottomin the fringe and empty lines
-(setq-default indicate-buffer-boundaries nil)
-(setq-default indicate-empty-lines nil)
-
-;; Continue wrapped lines at whitespace rather than breaking in the
-;; middle of a word.
 (setq-default word-wrap t)
-
-;; Disable wrapping by default due to its performance cost.
 (setq-default truncate-lines t)
 
-;; If enabled and `truncate-lines' is disabled, soft wrapping will not occur
-;; when the window is narrower than `truncate-partial-width-windows' characters.
 (setq truncate-partial-width-windows nil)
 
-;; Prefer spaces over tabs. Spaces offer a more consistent default compared to
-;; 8-space tabs. This setting can be adjusted on a per-mode basis as needed.
 (setq-default indent-tabs-mode nil
               tab-width 4)
 
+(menu-bar-mode -1)            ; no menu bar
+(tool-bar-mode -1)            ; no tools bar
+(scroll-bar-mode -1)          ; no scroll bars
+(tooltip-mode -1)             ; no tooltips
+(set-fringe-mode 10)          ; frame edges set to 10px
+(column-number-mode 1)        ; modeline shows column number
+(save-place-mode 1)           ; remember cursor position
+(recentf-mode 1)              ; remember recent files
+(savehist-mode 1)             ; enable history saving
+(electric-pair-mode 1)        ; enable keep pairs balanced
+(global-auto-revert-mode t)   ; Load external changes
+(global-hl-line-mode 1)       ; Current line highlight
+(delete-selection-mode t)     ; Overwrite/delete selected text
+
 (setq-default tab-always-indent t)
-
-;; We often split terminals and editor windows or place them side-by-side,
-;; making use of the additional horizontal space.
 (setq-default fill-column 80)
-
-;; Disable the obsolete practice of end-of-line spacing from the
-;; typewriter era.
 (setq sentence-end-double-space nil)
-
-;; According to the POSIX, a line is defined as "a sequence of zero or
-;; more non-newline characters followed by a terminating newline".
 (setq require-final-newline t)
-
-;; Remove duplicates from the kill ring to reduce clutter
 (setq kill-do-not-save-duplicates t)
-
-;; Ensures that empty lines within the commented region are also commented out.
-;; This prevents unintended visual gaps and maintains a consistent appearance,
-;; ensuring that comments apply uniformly to all lines, including those that are
-;; otherwise empty.
 (setq comment-empty-lines t)
 
-;;; Mode line
-
-;; Setting `display-time-default-load-average' to nil makes Emacs omit the load
-;; average information from the mode line.
-(setq display-time-default-load-average nil)
-
-;; Display the current line and column numbers in the mode line
-(setq line-number-mode t)
-(setq column-number-mode t)
-
-(setq display-line-numbers-type 'relative)
 (dolist (mode '(prog-mode-hook
                 conf-mode-hook))
   (add-hook mode (lambda () (display-line-numbers-mode 1))))
 
 (add-hook 'dired-mode-hook 'auto-revert-mode)
 
-;; (setenv "PATH" (concat (getenv "PATH") ":~/.asdf/shims"))
-;; (setq exec-path (append exec-path '("~/.asdf/shims")))
+(set-face-attribute 'default nil :font "BerkeleyMono Nerd Font 13")
+(set-face-attribute 'variable-pitch nil :font "BerkeleyMono Nerd Font 13")
+(set-face-attribute 'fixed-pitch nil :font "BerkeleyMono Nerd Font 13")
 
-;;; Load post-init.el
-(minimal-emacs-load-user-init "post-init.el")
+(use-package doom-themes
+  :init
+  (setq doom-themes-enable-bold t
+        doom-themes-enable-italic t)
+  (load-theme 'doom-moonlight t)
+  (doom-themes-org-config))
 
-(provide 'init)
+(use-package vertico
+  :bind (:map vertico-map
+              ("C-j" . vertico-next)
+              ("C-k" . vertico-previous)
+              ("C-f" . vertico-exit)
+              :map minibuffer-local-map
+              ("M-h" . backward-kill-word))
+  :custom
+  (vertico-cycle t)
+  :init
+  (vertico-mode))
 
-;;; init.el ends here
+(use-package marginalia
+  :after vertico
+  :custom
+  (marginalia-annotators '(marginalia-annotators-heavy marginalia-annotators-light nil))
+  :init
+  (marginalia-mode))
+
+(use-package company
+  :bind (:map company-active-map
+              ("<return>". nil)
+              ("RET" . nil)
+              ("C-<return>" . company-complete-selection)
+              ("C-y" . company-complete-selection))
+  :custom
+  (company-minimum-prefix-length 4)
+  :hook (after-init . global-company-mode))
+
+(use-package apheleia
+  :init
+  (apheleia-global-mode +1)
+  :hook
+  (go-mode . (lambda() (setq apheleia-inhibit t))))
+
+(use-package which-key
+  :custom
+  (which-key-side-window-location 'bottom)
+  :init
+  (which-key-mode))
+
+(use-package magit
+  :commands magit)
+
+(use-package consult
+  :bind (("C-c f f" . consult-fd)
+         ("C-c f b" . consult-buffer)
+         ("C-c s g" . consult-ripgrep)
+         ("C-c s l" . consult-line)))
+
+(use-package orderless
+  :custom
+  (completion-styles '(orderless basic))
+  (completion-avy-goto-char-timercategory-overrides '((file (styles basic partial-completion)))))
+
+(use-package ace-window
+  :custom
+  (aw-dispatch-always t)
+  :bind ("C-c w" . ace-window))
+
+(use-package avy
+  :bind (("C-c j" . avy-goto-char-2)
+         ("C-c l" . avy-goto-line)))
+
+(use-package expand-region
+  :bind ("C-=" . er/expand-region))
+
+(use-package tree-sitter
+  :init
+  (global-tree-sitter-mode)
+  :hook
+  (tree-sitter-after-on . tree-sitter-hl-mode))
+
+(use-package tree-sitter-langs
+  :after tree-sitter)
+
+(use-package go-mode
+  :after tree-sitter)
+
+(use-package json-mode
+  :after tree-sitter)
+
+(use-package typescript-mode
+  :after tree-sitter
+  :init
+  (define-derived-mode typescriptreact-mode typescript-mode
+    "TypeScript TSX")
+  (add-to-list 'auto-mode-alist '("\\.tsx?\\'" . typescriptreact-mode))
+  (add-to-list 'tree-sitter-major-mode-language-alist '(typescriptreact-mode . tsx)))
+
+(defun crnvl/eglot-typescript-setup ()
+  "Configure flycheck for a better TypeScript (eslint) experience."
+  (eglot-ensure))
+
+(use-package eglot
+  :hook
+  (typescript-mode . crnvl/eglot-typescript-setup))
