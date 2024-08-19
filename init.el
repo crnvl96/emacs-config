e38efdbd118ac3b6a74e0e773036ba8aa10a9481
Ádran Farias Carnavale <adran.carnavale@gmail.com>
2024-08-19 19:34:21 -0300

    feat: updts

--- init.el
+++ init.el
@@ -104,8 +104,12 @@
   (vertico-mode))
 
 (use-package golden-ratio-scroll-screen
-  :bind (("C-v" . golden-ratio-scroll-screen-up)
-         ("M-v" . golden-ratio-scroll-screen-down)))
+  :bind (("C-v" . (lambda () (interactive)
+                    (golden-ratio-scroll-screen-up)
+                    (recenter)))
+         ("M-v" . (lambda () (interactive)
+                    (golden-ratio-scroll-screen-down)
+                    (recenter)))))
 
 (use-package avy
   :bind (("M-s" . avy-goto-char-2)))
@@ -212,16 +216,33 @@
   (add-to-list 'auto-mode-alist '("\\.tsx?\\'" . typescriptreact-mode))
   (add-to-list 'tree-sitter-major-mode-language-alist '(typescriptreact-mode . tsx)))
 
-; (use-package flycheck
-;   :init
-;   (global-flycheck-mode))
-; :config
-; (flycheck-add-mode 'javascript-eslint 'typescriptreact-mode))
+(defun crnvl/use-eslint-from-node-modules ()
+  "Load eslint from node_modules."
+  (let* ((root (locate-dominating-file
+                (or (buffer-file-name) default-directory)
+                "node_modules"))
+         (eslint
+          (and root
+               (expand-file-name "node_modules/.bin/eslint"
+                                 root))))
+    (when (and eslint (file-executable-p eslint))
+      (setq-local flycheck-javascript-eslint-executable eslint))))
 
-(defun crnvl/eglot-typescript-setup ()
-  "Configure eglot for a better TypeScript (eslint) experience."
-  (eglot-ensure))
+(use-package flycheck
+  :init
+  (global-flycheck-mode)
+  ;; (flycheck-add-mode 'javascript-eslint 'typescriptreact-mode)
+  :custom
+  (flycheck-idle-change-delay 0)
+  (setq-local flycheck-check-syntax-automatically '(save)))
 
-(use-package eglot
-  :hook
-  (typescript-mode . crnvl/eglot-typescript-setup))
+(use-package lsp-mode
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :hook ((typescriptreact-mode . lsp)
+         (lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp
+  :custom
+  (lsp-idle-delay 0))
+
+(use-package lsp-ui :commands lsp-ui-mode)
